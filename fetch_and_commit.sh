c236d6947fbd7bcb67a9ce7740e8a97401a62c37
IrtyGo <77201294+irtygo@users.noreply.github.com>
2025-03-05 22:54:22 +0300

    Update fetch_and_commit.sh

--- fetch_and_commit.sh
+++ fetch_and_commit.sh
@@ -14,15 +14,27 @@ cd out
 
 echo "Downloading the livecd iso"
 
-curl https://iso.reactos.org/livecd/latest-x86-gcc-lin-rel -O out/reactos_livecd_latest.iso
+curl https://iso.reactos.org/livecd/latest-x86-gcc-lin-rel -O out/reactos_livecd_latest.7z
+
+echo "Decompressing the livecd"
+
+7za x *.7z
+rm -r *.7z
+mv *livecd*.iso reactos_livecd_latest.iso
 
 echo "Downloading the bootcd iso"
 
 curl https://iso.reactos.org/bootcd/latest-x86-gcc-lin-rel -O out/reactos_bootcd_latest.iso
 
+echo "Decompressing the bootcd"
+
+7za x *.7z
+rm -r *.7z
+mv *bootcd*.iso reactos_bootcd_latest.iso
+
 git config --global user.name "irty-auto-ros"
 git config --global user.email "github-actions@github.com"
-git add artifact.txt
+git add *.iso
 git commit -m $(printf "Update the ISOs at %s" $(date +"%a %d/%m/%Y"))
 git push
 
